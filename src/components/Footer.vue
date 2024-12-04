5746b84413f1b2b94a7f1540b8cf00b0d55e42aa
Andi <andimilyas123@gmail.com>
2024-12-04 09:45:43 +0700

    install vue and tailwind

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template> 
+    
+</template>
+
+<script setup> 
+import MainLogo from "./icon/MainLogo.vue";
+ 
+</script>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./styles/index.css";
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router"; 
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+const app = createApp(App);
+
+app
+    .component("Navbar", Navbar)
+    .component("Footer", Footer)
+    
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
